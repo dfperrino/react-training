2cf0aedab2f8b430821cbda67e3eba4ea2585b84
Vega Alonso, Ricardo <rvalonso@minsait.com>
2021-06-18 14:01:31 +0200

    refactor: componetize App

--- src/App.js
+++ src/App.js
@@ -7,40 +7,50 @@ import ErrorCatcher from "./components/ErrorCatcher";
 import { useState } from "react";
 import { allergens } from "./data";
 
-function App() {
+const Content = () => {
   const [menu, setMenu] = useState([]);
-  const [isFemale, setIsFemale] = useState(false);
-
-  const title = "React Training";
-
   const handleAdd = (id) => {
     setMenu((pv) => [...pv, allergens.find((a) => a.id === id)]);
   };
+  return (
+    <div className="flex w-100">
+      <div className="w-50">
+        <h1>Available:</h1>
+        <List items={allergens} onSelect={handleAdd} showButtons />
+      </div>
+      <div className="w-50">
+        <h1>Menu:</h1>
+        <List items={menu} />
+      </div>
+    </div>
+  );
+};
+
+const SubHeader = () => {
+  return (
+    <>
+      <img src={logo} className="App-logo" alt="logo" />
+      <ErrorCatcher>
+        <BugComponent />
+      </ErrorCatcher>
+    </>
+  );
+};
+
+const App = () => {
+  const [isFemale, setIsFemale] = useState(false);
+
+  const title = "React Training";
 
   const handleHeaderClick = () => setIsFemale(!isFemale);
 
   return (
     <div className="App">
       <Header title={title} isFemale={isFemale} onClick={handleHeaderClick} />
-      <>
-        <img src={logo} className="App-logo" alt="logo" />
-        <ErrorCatcher>
-          <BugComponent />
-        </ErrorCatcher>
-      </>
-
-      <div className="flex w-100">
-        <div className="w-50">
-          <h1>Available:</h1>
-          <List items={allergens} onSelect={handleAdd} showButtons />
-        </div>
-        <div className="w-50">
-          <h1>Menu:</h1>
-          <List items={menu} />
-        </div>
-      </div>
+      <SubHeader />
+      <Content />
     </div>
   );
-}
+};
 
 export default App;
