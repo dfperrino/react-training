0190e63b1db6e7b9e96bfaa1212d4721028eed74
Vega Alonso, Ricardo <rvalonso@minsait.com>
2021-06-18 12:40:32 +0200

    feat: add heavy calculation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,12 @@
 import React, { useEffect } from "react";
 
+const heavyCalc = () => {
+  for (let index = 0; index < 1e8; index++) {
+    // nothing
+  }
+  return 0;
+};
+
 const Header = (props) => {
   // On mount
   useEffect(() => {
@@ -10,6 +17,16 @@ const Header = (props) => {
   useEffect(() => {
     window.alert("update");
   }, [props.title]);
+
+  const calculateValue = () => {
+    const t0 = performance.now();
+    const value = heavyCalc();
+    const t1 = performance.now();
+    console.log("La llamada heavyCalc tardó " + (t1 - t0) + " milisegundos.");
+    return value;
+  };
+
+  const calculatedValue = calculateValue();
 
   return (
     <header onClick={props.onClick}>
