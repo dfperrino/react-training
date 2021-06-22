70ff7c04cb915c6f6ab226911f7307edca7c3769
blendoiro <>
2021-06-22 11:09:12 +0200

    feat: axios call

--- src/components/HelpDetail.js
+++ src/components/HelpDetail.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import axios from 'axios';
 
 const HelpDetail = (props) => {
   const params = useParams();
+  const [helpData, setHelpData] = useState('');
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    axios.get(`http://localhost:4000/help/${params.name}`)
+    .then((res) => {
+      setHelpData(res.data.data);
+    })
+    .finally(() => {
+      setLoading(false);
+    })
+  }, [params.name]);
+
   return (
     <div>
      <h3>{params.name}</h3>
-     <p>Here we are going to show {params.name} help</p>
+     <p>{loading && 'The info is loading'}</p>
+     <p>{!loading && helpData}</p>
     </div>
   );
 };
